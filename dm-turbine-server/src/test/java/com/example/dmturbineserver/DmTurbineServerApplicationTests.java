3a1e69e5b3b9d8220f218c984edfb06a49e84d0c
soviet <2570054539@qq.com>
2019-06-05 17:25:16 +0800

    🦌

---
+++ dm-turbine-server/src/test/java/com/example/dmturbineserver/DmTurbineServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.dmturbineserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DmTurbineServerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
